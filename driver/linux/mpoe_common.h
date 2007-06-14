e9207d5019f57f57d4889a9b763781479fe7f2a3
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2007-06-14 12:25:13 +0000

    add comments in all function prototypes in mpoe_common.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@64 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- driver/linux/mpoe_common.h
+++ driver/linux/mpoe_common.h
@@ -50,33 +50,39 @@ struct mpoe_user_region {
 	} segments[0];
 };
 
+/* manage endpoints */
 extern int mpoe_net_attach_endpoint(struct mpoe_endpoint * endpoint, uint8_t board_index, uint8_t endpoint_index);
 extern void mpoe_net_detach_endpoint(struct mpoe_endpoint * endpoint);
 extern int mpoe_close_endpoint(struct mpoe_endpoint * endpoint, void __user * dummy);
 
+/* manage ifaces */
 extern int mpoe_net_ifaces_show(char *buf);
 extern int mpoe_net_ifaces_store(const char *buf, size_t size);
-
 extern int mpoe_net_get_iface_count(void);
 extern int mpoe_net_get_iface_id(uint8_t board_index, struct mpoe_mac_addr * board_addr, char * board_name);
 extern struct mpoe_iface * mpoe_net_iface_from_ifp(struct net_device *ifp);
 
+/* sending */
 extern int mpoe_net_send_tiny(struct mpoe_endpoint * endpoint, void __user * uparam);
 extern int mpoe_net_send_medium(struct mpoe_endpoint * endpoint, void __user * uparam);
 extern int mpoe_net_send_rendez_vous(struct mpoe_endpoint * endpoint, void __user * uparam);
 extern int mpoe_net_send_pull(struct mpoe_endpoint * endpoint, void __user * uparam);
 extern int mpoe_net_pull_reply(struct mpoe_endpoint * endpoint, struct mpoe_pkt_pull_request * pull_request, struct mpoe_mac_addr * dest_addr);
 
+/* receiving */
 extern int mpoe_net_recv(struct sk_buff *skb, struct net_device *ifp, struct packet_type *pt, struct net_device *orig_dev);
 
+/* user regions */
 extern void mpoe_init_endpoint_user_regions(struct mpoe_endpoint * endpoint);
 extern int mpoe_register_user_region(struct mpoe_endpoint * endpoint, void __user * uparam);
 extern int mpoe_deregister_user_region(struct mpoe_endpoint * endpoint, void __user * uparam);
 extern void mpoe_deregister_endpoint_user_regions(struct mpoe_endpoint * endpoint);
 
+/* device */
 extern int mpoe_dev_init(void);
 extern void mpoe_dev_exit(void);
 
+/* manage addresses */
 static inline void
 mpoe_mac_addr_of_netdevice(struct net_device * ifp,
 			   struct mpoe_mac_addr * mpoe_addr)
