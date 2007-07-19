8d74936ec63d06a291ec564aab3a958172291090
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2007-07-19 11:00:44 +0000

    add mpoe__get_board_index_by_name
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@224 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- libmpoe/mpoe_get_info.c
+++ libmpoe/mpoe_get_info.c
@@ -99,3 +99,49 @@ mpoe__get_board_id(struct mpoe_endpoint * ep, uint8_t * index,
  out:
   return ret;
 }
+
+/* returns the current index of a board given by its name */
+mpoe_return_t
+mpoe__get_board_index_by_name(const char * name, uint8_t * index)
+{
+  mpoe_return_t ret = MPOE_SUCCESS;
+  uint32_t max;
+  int err, fd, i;
+
+  err = open(MPOE_DEVNAME, O_RDONLY);
+  if (err < 0) {
+    ret = mpoe__errno_to_return(errno, "open");
+    goto out;
+  }
+  fd = err;
+
+  err = ioctl(fd, MPOE_CMD_GET_BOARD_MAX, &max);
+  if (err < 0) {
+    ret = mpoe__errno_to_return(errno, "ioctl GET_BOARD_MAX");
+    goto out_with_fd;
+  }
+
+  ret = MPOE_INVALID_PARAMETER;
+  for(i=0; i<max; i++) {
+    struct mpoe_cmd_get_board_id board_id;
+
+    board_id.board_index = i;
+    err = ioctl(fd, MPOE_CMD_GET_BOARD_ID, &board_id);
+    if (err < 0) {
+      ret = mpoe__errno_to_return(errno, "ioctl GET_BOARD_ID");
+      if (ret != MPOE_INVALID_PARAMETER)
+	goto out_with_fd;
+    }
+
+    if (!strncmp(name, board_id.board_name, MPOE_IF_NAMESIZE)) {
+      ret = MPOE_SUCCESS;
+      *index = i;
+      break;
+    }
+  }
+
+ out_with_fd:
+  close(fd);
+ out:
+  return ret;
+}
