ab69af8916c79ddda13b22c262d5c8207f768f80
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2007-07-26 13:50:45 +0000

    add copyright/license headers in all source files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@320 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- driver/linux/omx_pull.c
+++ driver/linux/omx_pull.c
@@ -1,3 +1,21 @@
+/*
+ * Open-MX
+ * Copyright © INRIA 2007 (see AUTHORS file)
+ *
+ * The development of this software has been funded by Myricom, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License in COPYING.GPL for more details.
+ */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/slab.h>
