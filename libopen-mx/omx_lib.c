344826fe05fd3bc471060b85d4f187b2cf31a529
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2007-08-02 14:28:20 +0000

    move errno/strfoo to omx_misc.c
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@374 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- libopen-mx/omx_lib.c
+++ libopen-mx/omx_lib.c
@@ -19,83 +19,11 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <assert.h>
 
 #include "omx_io.h"
 #include "omx_lib.h"
 #include "omx_request.h"
-
-/***********************
- * Management of errors
- */
-
-omx_return_t
-omx__errno_to_return(char * caller)
-{
-  switch (errno) {
-  case EINVAL:
-    return OMX_INVALID_PARAMETER;
-  case EACCES:
-  case EPERM:
-    return OMX_ACCESS_DENIED;
-  case EMFILE:
-  case ENFILE:
-  case ENOMEM:
-    return OMX_NO_SYSTEM_RESOURCES;
-  case ENODEV:
-  case ENOENT:
-    return OMX_NO_DEVICE;
-  default:
-    fprintf(stderr, "Open-MX: %s got unexpected errno %d (%m)\n",
-	    caller, errno);
-    return OMX_BAD_ERROR;
-  }
-}
-
-const char *
-omx_strerror(omx_return_t ret)
-{
-  switch (ret) {
-  case OMX_SUCCESS:
-    return "Success";
-  case OMX_BAD_ERROR:
-    return "Bad (internal?) error";
-  case OMX_ALREADY_INITIALIZED:
-    return "Already initialized";
-  case OMX_NOT_INITIALIZED:
-    return "Not initialized";
-  case OMX_NO_DEVICE:
-    return "No device";
-  case OMX_ACCESS_DENIED:
-    return "Access denied";
-  case OMX_NO_RESOURCES:
-    return "No resources available";
-  case OMX_NO_SYSTEM_RESOURCES:
-    return "No resources available in the system";
-  case OMX_INVALID_PARAMETER:
-    return "Invalid parameter";
-  case OMX_NOT_IMPLEMENTED:
-    return "Not implemented";
-  case OMX_BAD_CONNECTION_KEY:
-    return "Bad Connection Key";
-  }
-  assert(0);
-}
-
-const char *
-omx_strstatus(omx_status_code_t code)
-{
-  switch (code) {
-  case OMX_STATUS_SUCCESS:
-    return "Success";
-  case OMX_STATUS_FAILED:
-    return "Failed";
-  case OMX_STATUS_BAD_KEY:
-    return "Bad Connection Key";
-  }
-  assert(0);
-}
 
 /****************
  * Early packets
