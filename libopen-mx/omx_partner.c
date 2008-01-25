d765c8ec3b2979169b3bd7bf68c9ca9ded6cd472
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2008-01-25 12:33:34 +0000

    fix initialization for the self endpoint partner localization
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1153 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- libopen-mx/omx_partner.c
+++ libopen-mx/omx_partner.c
@@ -214,6 +214,8 @@ omx__connect_myself(struct omx_endpoint *ep, uint64_t board_addr)
   ep->myself->next_acked_send_seq = OMX__SEQNUM(1);
   ep->myself->true_session_id = ep->desc->session_id;
   ep->myself->back_session_id = ep->desc->session_id;
+  ep->myself->localization = (omx__globals.sharedcomms || omx__globals.selfcomms)
+	? OMX__PARTNER_LOCALIZATION_LOCAL : OMX__PARTNER_LOCALIZATION_REMOTE;
 
   return OMX_SUCCESS;
 }
