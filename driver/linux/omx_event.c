754f0c520d261232d6d0403d161ab3601fb9b480
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2008-02-20 10:57:59 +0000

    Fix a race condition in the driver wakeup code between omx_wakeup_on_event() and the cleanup in omx_ioctl_wait_event().
    The whole waiter status+list_elt must be kept coherent. We can't check the status
    and remove from the list without holding the lock during the whole time.
    
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1333 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- driver/linux/omx_event.c
+++ driver/linux/omx_event.c
@@ -379,12 +379,15 @@ omx_ioctl_wait_event(struct omx_endpoint * endpoint, void __user * uparam)
 		del_singleshot_timer_sync(&timer);
 
  wakeup:
-	if (waiter.status != OMX_CMD_WAIT_EVENT_STATUS_EVENT) {
+	spin_lock_bh(&endpoint->event_lock);
+	/* always need to lock even if we'll do nothing below
+	 * since the whole status+list_elt need to be coherent
+	 * to avoid race conditions with omx_wakeup_on_event()
+	 */
+	if (waiter.status != OMX_CMD_WAIT_EVENT_STATUS_EVENT)
 		/* no event removed us from the wait queue, do it now */
-		spin_lock_bh(&endpoint->event_lock);
 		list_del(&waiter.list_elt);
-		spin_unlock_bh(&endpoint->event_lock);
-	}
+	spin_unlock_bh(&endpoint->event_lock);
 
 	if (waiter.status == OMX_CMD_WAIT_EVENT_STATUS_NONE) {
 		/* status didn't changed, we have been interrupted */
