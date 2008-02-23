67624a75d926dfad8bee56ec00cd46afe57f6798
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2008-02-23 08:40:58 +0000

    Make sure we set the task back to TASK_RUNNING when not really sleeping.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1360 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- driver/linux/omx_event.c
+++ driver/linux/omx_event.c
@@ -379,6 +379,8 @@ omx_ioctl_wait_event(struct omx_endpoint * endpoint, void __user * uparam)
 		del_singleshot_timer_sync(&timer);
 
  wakeup:
+	set_current_state(TASK_RUNNING);
+
 	spin_lock_bh(&endpoint->event_lock);
 	list_del(&waiter.list_elt);
 	spin_unlock_bh(&endpoint->event_lock);
