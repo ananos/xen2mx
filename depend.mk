925c4069fc69d6b58cf69224a5c11a79f0dc458e
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2008-03-10 16:34:47 +0000

    Cleanup the build output by display CC/LD/... and the target file instead of the whole compile line (it make warnings way more visible).
    make V=1 will display the whole compile line as earlier.
    
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1472 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- depend.mk
+++ depend.mk
@@ -10,12 +10,12 @@ ONLYDEPFLAGS	=	-MMD -MM
 # create a dependency file when it doesn't exist (called by the include line below)
 ifeq ($(BUILD_O_AND_LO_OBJECTS),1)
 $(DEPS): %.d: $(DEPSRCDIR)/%.c
-	@echo "Generating dependency file for $(subst .d,.c,$@)"
-	@$(CC) $(CPPFLAGS) $(ONLYDEPFLAGS) $(DEPSRCDIR)/$(subst .d,.c,$@) -MT $(subst .d,.o,$@) -MT $(subst .d,.lo,$@)
+	$(CMDECHO) "  DEP     $(REL_DIR)$@"
+	$(CMDPREFIX) $(CC) $(CPPFLAGS) $(ONLYDEPFLAGS) $(DEPSRCDIR)/$(subst .d,.c,$@) -MT $(subst .d,.o,$@) -MT $(subst .d,.lo,$@)
 else
 $(DEPS): %.d: $(DEPSRCDIR)/%.c
-	@echo "Generating dependency file for $(subst .d,.c,$@)"
-	@$(CC) $(CPPFLAGS) $(ONLYDEPFLAGS) $(DEPSRCDIR)/$(subst .d,.c,$@)
+	$(CMDECHO) "  DEP     $(REL_DIR)$@"
+	$(CMDPREFIX) $(CC) $(CPPFLAGS) $(ONLYDEPFLAGS) $(DEPSRCDIR)/$(subst .d,.c,$@)
 endif
 
 # include dependencies, except when cleaning (so that we don't re-create for nothing)
