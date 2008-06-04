252e46a139cef3399b6c6033ed459a1e3b7e0b07
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2008-06-04 08:10:23 +0000

    Use driver feature "wirecompat" flag to check whether we need to ack immediately after receiving a notify. The lib is now totally wire-compat independent.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1743 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- libopen-mx/omx_large.c
+++ libopen-mx/omx_large.c
@@ -591,13 +591,13 @@ omx__process_recv_notify(struct omx_endpoint *ep, struct omx__partner *partner,
     omx__send_complete(ep, req, OMX_SUCCESS);
   }
 
-#ifdef OMX_MX_WIRE_COMPAT
-  /* MX < 1.2.5 needs an immediate ack for notify since it cannot mark
-   * large recv as zombies.
-   * But we can only do that if all previous seqnum are ready to be acked
-   * too, which means next_frag == next_match.
-   */
-  if (partner->next_frag_recv_seq == partner->next_match_recv_seq)
-    omx__mark_partner_need_ack_immediate(ep, partner);
-#endif
+  if (omx__driver_desc->features & OMX_DRIVER_FEATURE_WIRECOMPAT) {
+    /* MX < 1.2.5 needs an immediate ack for notify since it cannot mark
+     * large recv as zombies.
+     * But we can only do that if all previous seqnum are ready to be acked
+     * too, which means next_frag == next_match.
+     */
+    if (partner->next_frag_recv_seq == partner->next_match_recv_seq)
+      omx__mark_partner_need_ack_immediate(ep, partner);
+  }
 }
