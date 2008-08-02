ananos/xen2mx

commit 300de14113d1778286988710b1e834c4795d55c4

Always add the iface to the peer table
... in omx_peers_notify_iface_attach() even it wasn't listed as a regular
peer already.


git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1823 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

driver/linux/omx_peer.c
@@ -266,12 +266,14 @@ omx_peer_add(uint64_t board_addr, char *hostname)
  * Local Iface Management
  */
 
-void
+int
 omx_peers_notify_iface_attach(struct omx_iface * iface)
 {
 	struct omx_peer * oldpeer, * ifacepeer;
 	uint64_t board_addr;
+	uint32_t index;
 	uint8_t hash;
+	int err;
 
 	ifacepeer = &iface->peer;
 	board_addr = ifacepeer->board_addr;
@@ -281,7 +283,10 @@ omx_peers_notify_iface_attach(struct omx_iface * iface)
 
 	list_for_each_entry(oldpeer, &omx_peer_addr_hash_array[hash], addr_hash_elt) {
 		if (oldpeer->board_addr == board_addr) {
-			uint32_t index = oldpeer->index;
+
+			/* the peer is already in the table, replace it */
+
+			index = oldpeer->index;
 
 			dprintk(PEER, "attaching local iface %s (%s) with address %012llx as peer #%d %s\n",
 				iface->eth_ifp->name, ifacepeer->hostname, (unsigned long long) board_addr,
@@ -313,11 +318,45 @@ omx_peers_notify_iface_attach(struct omx_iface * iface)
 			rcu_assign_pointer(omx_peer_array[index], ifacepeer);
 			list_replace_rcu(&oldpeer->addr_hash_elt, &ifacepeer->addr_hash_elt);
 			call_rcu(&oldpeer->rcu_head, __omx_peer_rcu_free_callback);
-			break;
+
+			mutex_unlock(&omx_peers_mutex);
+			return 0;
 		}
 	}
 
+	/* the iface is not in the peer table yet, add it */
+
+	err = -ENOMEM;
+	if (omx_peer_next_nr == omx_peer_max)
+		goto out_with_mutex;
+
+	/* this is a new peer, allocate an index and hash it */
+	index = omx_peer_next_nr;
+	list_add_tail_rcu(&ifacepeer->addr_hash_elt, &omx_peer_addr_hash_array[hash]);
+	rcu_assign_pointer(omx_peer_array[omx_peer_next_nr], ifacepeer);
+	omx_peer_next_nr++;
+
+	/* board_addr already set */
+	ifacepeer->local_iface = iface;
+	ifacepeer->index = index;
+	ifacepeer->reverse_index = index;
+
+	dprintk(PEER, "attaching local iface %s (%s) with address %012llx as new peer #%d\n",
+		iface->eth_ifp->name, ifacepeer->hostname, (unsigned long long) board_addr, index);
+
+	/* take a reference on the iface */
+	omx_iface_reacquire(iface);
+
+	/* no need to host query */
+
+	rcu_assign_pointer(omx_peer_array[index], ifacepeer);
+
 	mutex_unlock(&omx_peers_mutex);
+	return 0;
+
+ out_with_mutex:
+	mutex_unlock(&omx_peers_mutex);
+	return err;
 }
 
 void

driver/linux/omx_peer.h
@@ -29,7 +29,7 @@ extern int omx_peers_init(void);
 extern void omx_peers_exit(void);
 extern void omx_peers_clear(int local);
 extern void omx_peers_clear_names(void);
-extern void omx_peers_notify_iface_attach(struct omx_iface * iface);
+extern int omx_peers_notify_iface_attach(struct omx_iface * iface);
 extern void omx_peers_notify_iface_detach(struct omx_iface * iface);
 extern int omx_peer_add(uint64_t board_addr, char *hostname);
 extern void omx_peer_set_reverse_index_locked(struct omx_peer *peer, uint16_t reverse_index);

commit cc9aa1a6be214a89795cd80f15ab1744047104eb

Do not take a reference onto the iface when add its peer to the table if it was already there.
Regression introduced by commit r1823
    Always add the iface to the peer table
    ... in omx_peers_notify_iface_attach() even it wasn't listed as a regular
    peer already.

git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1890 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

driver/linux/omx_peer.c
@@ -115,6 +115,7 @@ omx_peers_clear(int local)
 			/* local iface peer hostname cannot be NULL, no need to update the host_query_list or so */
 
 			peer->index = OMX_UNKNOWN_REVERSE_PEER_INDEX;
+			peer->local_iface = NULL;
 
 			/* release the iface reference now it is not linked in the peer table anymore */
 			omx_iface_release(iface);
@@ -174,9 +175,16 @@ omx_peer_add(uint64_t board_addr, char *hostname)
 	if (iface) {
 		/* add local iface to peer table and update the iface name if possible */
 
-		/* omx_iface_find_by_addr() acquired the iface, keep the reference */
 		peer = &iface->peer;
-		peer->local_iface = iface;
+
+		if (peer->index != OMX_UNKNOWN_REVERSE_PEER_INDEX) {
+			/* the iface was already in the table, no need to keep the reference that omx_iface_find_by_addr() acquired */
+			omx_iface_release(iface);
+			BUG_ON(peer->local_iface != iface);
+		} else {
+			BUG_ON(peer->local_iface != NULL);
+			peer->local_iface = iface;
+		}
 
 		/* replace the iface hostname with the one from the peer table if non-null */
 		if (new_hostname) {
@@ -379,7 +387,10 @@ omx_peers_notify_iface_detach(struct omx_iface * iface)
 		rcu_assign_pointer(omx_peer_array[index], NULL);
 		/* no need to bother using call_rcu() here, waiting a bit long in synchronize_rcu() is ok */
 		synchronize_rcu();
+
+		/* mark the iface as not in the table */
 		peer->index = OMX_UNKNOWN_REVERSE_PEER_INDEX;
+		peer->local_iface = NULL;
 
 		/* release the iface reference now it is not linked in the peer table anymore */
 		omx_iface_release(iface);