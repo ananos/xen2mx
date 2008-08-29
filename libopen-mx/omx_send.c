ananos/xen2mx

commit 6657e5ab011ab53387b80b1cc7d2c9329b91291b

Drop the resend_req_q and the RESENDING state since it is just a staging area between the general non_acked_req_q and another queue after the reposting to the driver. It brings nothing more than complexity without helping for anything since nothing else can happen while some requests are on it.
Just always put the requests on non_acked queue (except when waiting for
a driver event of course), they are ordered by next resend time, we can
just resend the first ones up to one whose resend delay hasn't expired
yet.



git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@1906 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

libopen-mx/omx_send.c
@@ -1023,19 +1023,12 @@ omx__process_resend_requests(struct omx_endpoint *ep)
   union omx_request *req, *next;
   uint64_t now = omx__driver_desc->jiffies;
 
-  /* move non acked requests to the resend_req_q */
+  /* resend the first requests from the non_acked queue */
   omx__foreach_request_safe(&ep->non_acked_req_q, req, next) {
     if (now - req->generic.last_send_jiffies < omx__globals.resend_delay_jiffies)
       /* the remaining ones are more recent, no need to resend them yet */
       break;
 
-    omx___dequeue_request(req);
-    req->generic.state |= OMX_REQUEST_STATE_RESENDING;
-    omx__enqueue_request(&ep->resend_req_q, req);
-  }
-
-  /* resend requests from the resend_req_q */
-  omx__foreach_request_safe(&ep->resend_req_q, req, next) {
     /* check before dequeueing so that omx__partner_cleanup() is called with queues in a coherent state */
     if (req->generic.resends > req->generic.resends_max) {
       /* Disconnect the peer (and drop the requests) */
@@ -1047,7 +1040,6 @@ omx__process_resend_requests(struct omx_endpoint *ep)
       continue;
     }
 
-    req->generic.state &= ~OMX_REQUEST_STATE_RESENDING;
     omx___dequeue_request(req);
 
     switch (req->generic.type) {

libopen-mx/omx_ack.c
@@ -32,21 +32,14 @@ omx__mark_request_acked(struct omx_endpoint *ep,
 			union omx_request *req,
 			omx_return_t status)
 {
-  struct list_head *queue;
-
-  if (req->generic.state & OMX_REQUEST_STATE_RESENDING)
-    queue = &ep->resend_req_q;
-  else
-    queue = &ep->non_acked_req_q;
-
   omx__debug_assert(req->generic.state & OMX_REQUEST_STATE_NEED_ACK);
-  req->generic.state &= ~(OMX_REQUEST_STATE_NEED_ACK|OMX_REQUEST_STATE_RESENDING);
+  req->generic.state &= ~OMX_REQUEST_STATE_NEED_ACK;
 
   switch (req->generic.type) {
 
   case OMX_REQUEST_TYPE_SEND_TINY:
   case OMX_REQUEST_TYPE_SEND_SMALL:
-    omx__dequeue_request(queue, req);
+    omx__dequeue_request(&ep->non_acked_req_q, req);
     omx__send_complete(ep, req, status);
     break;
 
@@ -57,14 +50,14 @@ omx__mark_request_acked(struct omx_endpoint *ep,
 	/* set the status (success for ack, error for nack) only if there has been no error early */
 	req->generic.status.code = omx__error_with_req(ep, req, status, "Send request nacked");
     } else {
-      omx__dequeue_request(queue, req);
+      omx__dequeue_request(&ep->non_acked_req_q, req);
       omx__send_complete(ep, req, status);
     }
     break;
 
   case OMX_REQUEST_TYPE_SEND_LARGE:
     /* if the request was already replied, it would have been acked at the same time */
-    omx__dequeue_request(queue, req);
+    omx__dequeue_request(&ep->non_acked_req_q, req);
     if (unlikely(status != OMX_SUCCESS)) {
       /* the request has been nacked, there won't be any reply */
       req->generic.state &= ~OMX_REQUEST_STATE_NEED_REPLY;
@@ -78,7 +71,7 @@ omx__mark_request_acked(struct omx_endpoint *ep,
     break;
 
   case OMX_REQUEST_TYPE_RECV_LARGE:
-    omx__dequeue_request(queue, req);
+    omx__dequeue_request(&ep->non_acked_req_q, req);
     omx__recv_complete(ep, req, status);
     break;
 

libopen-mx/omx_endpoint.c
@@ -442,7 +442,6 @@ omx_open_endpoint(uint32_t board_index, uint32_t endpoint_index, uint32_t key,
   INIT_LIST_HEAD(&ep->pull_req_q);
   INIT_LIST_HEAD(&ep->connect_req_q);
   INIT_LIST_HEAD(&ep->non_acked_req_q);
-  INIT_LIST_HEAD(&ep->resend_req_q);
   INIT_LIST_HEAD(&ep->send_self_unexp_req_q);
 
   INIT_LIST_HEAD(&ep->partners_to_ack_immediate_list);

libopen-mx/omx_request.h
@@ -262,6 +262,12 @@ omx__dequeue_first_partner_throttling_request(struct omx__partner *partner)
   return req;
 }
 
+static inline int
+omx__empty_partner_throttling_request(struct omx__partner *partner)
+{
+  return list_empty(&partner->throttling_send_req_q);
+}
+
 #define omx__foreach_partner_throttling_request_safe(partner, req, next)		\
 list_for_each_entry_safe(req, next, &partner->throttling_send_req_q, generic.partner_elt)
 

libopen-mx/omx_types.h
@@ -255,8 +255,6 @@ struct omx_endpoint {
   struct list_head connect_req_q;
   /* any request that needs to be resent, thus NEED_ACK, and is not IN DRIVER (queued by their queue_elt) */
   struct list_head non_acked_req_q;
-  /* any request that needs to be resent now, thus RESENDING and NEED_ACK, not IN_DRIVER (queued by their queue_elt) */
-  struct list_head resend_req_q;
   /* send to self waiting for the matching */
   struct list_head send_self_unexp_req_q;
 
@@ -327,7 +325,6 @@ enum omx__request_type {
  * CONNECT:
  *   NEED_REPLY: connect_req_q
  *
- * if RESENDING added, resend_req_q instead of non_acked_req_q
  * if DELAYED, delayed_send_req_q
  *
  * The DONE qnd ZOMBIE states of the request determines whether the done_elt
@@ -353,9 +350,8 @@ enum omx__request_state {
   OMX_REQUEST_STATE_NEED_REPLY = (1<<5),
   /* needs a ack from the peer */
   OMX_REQUEST_STATE_NEED_ACK = (1<<6),
-  /* placed on a queue for resending through the driver soon */
-  OMX_REQUEST_STATE_RESENDING = (1<<7),
   /* request can already be completed by the application, even if not acked yet */
+
   OMX_REQUEST_STATE_DONE = (1<<8),
   /* request has been completed by the application and should not be notified when done for real (including acked) */
   OMX_REQUEST_STATE_ZOMBIE = (1<<9),

commit 72ecaf08bd43ac05562b27db310fb7ac000b4765

Fix regression introduced in commit r1906:   "Drop the resend_req_q and the RESENDING state"
Requeue to a temporary queue after resending, and append
the temp queue to the actual endpoint queue after exiting
the loop.
It prevents from entering infinite loop if jiffies changes
faster than we repost all non-acked request.



git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2003 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

libopen-mx/omx_send.c
@@ -1279,17 +1279,18 @@ omx__process_resend_requests(struct omx_endpoint *ep)
 {
   union omx_request *req, *next;
   uint64_t now = omx__driver_desc->jiffies;
+  LIST_HEAD(tmp_req_q);
 
   /* resend the first requests from the non_acked queue */
   omx__foreach_request_safe(&ep->non_acked_req_q, req, next) {
     if (now - req->generic.last_send_jiffies < omx__globals.resend_delay_jiffies)
       /* the remaining ones are more recent, no need to resend them yet */
-      break;
+      goto done_resending;
 
     /* check before dequeueing so that omx__partner_cleanup() is called with queues in a coherent state */
     if (req->generic.resends > req->generic.resends_max) {
       /* Disconnect the peer (and drop the requests) */
-      printf("Send request (seqnum %d sesnum %d)  timeout, already sent %ld times, resetting partner status\n",
+      printf("Send request (seqnum %d sesnum %d) timeout, already sent %ld times, resetting partner status\n",
 	     (unsigned) OMX__SEQNUM(req->generic.send_seqnum),
 	     (unsigned) OMX__SESNUM_SHIFTED(req->generic.send_seqnum),
 	     (unsigned long) req->generic.resends);
@@ -1346,16 +1347,17 @@ omx__process_resend_requests(struct omx_endpoint *ep)
     if (req->generic.state & OMX_REQUEST_STATE_DRIVER_MEDIUM_SENDING)
       omx__enqueue_request(&ep->driver_medium_sending_req_q, req);
     else
-      /* move the request to the end of the same queue */
-      omx__enqueue_request(&ep->non_acked_req_q, req);
+      omx__enqueue_request(&tmp_req_q, req);
   }
  done_resending:
+  /* requeue requests at the end */
+  list_splice_init(&tmp_req_q, ep->non_acked_req_q.prev);
 
   /* resend non-replied connect requests */
   omx__foreach_request_safe(&ep->connect_req_q, req, next) {
     if (now - req->generic.last_send_jiffies < omx__globals.resend_delay_jiffies)
       /* the remaining ones are more recent, no need to resend them yet */
-      break;
+      goto done_reconnecting;
 
     /* check before dequeueing so that omx__partner_cleanup() is called with queues in a coherent state */
     if (req->generic.resends > req->generic.resends_max) {
@@ -1368,6 +1370,11 @@ omx__process_resend_requests(struct omx_endpoint *ep)
     }
 
     /* no need to dequeue/requeue */
+    omx___dequeue_request(req);
     omx__post_connect_request(ep, req->generic.partner, req);
+    omx__enqueue_request(&tmp_req_q, req);
   }
+ done_reconnecting:
+  /* requeue requests at the end */
+  list_splice(&tmp_req_q, ep->connect_req_q.prev);
 }