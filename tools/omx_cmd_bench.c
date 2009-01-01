a97ba8fb5d13a0b69334cc885545ef911b1c6205
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2009-01-01 19:21:02 +0000

    update copyright to 2009
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2225 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- tools/omx_cmd_bench.c
+++ tools/omx_cmd_bench.c
@@ -1,6 +1,6 @@
 /*
  * Open-MX
- * Copyright © INRIA 2007-2008 (see AUTHORS file)
+ * Copyright © INRIA 2007-2009 (see AUTHORS file)
  *
  * The development of this software has been funded by Myricom, Inc.
  *
