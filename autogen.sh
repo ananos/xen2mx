bcdd1f5d89aae3daed4e9990559e16107c45e637
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2009-01-30 21:34:55 +0000

    Add autogen.sh to ease building from SVN. Just run ./autogen.sh with configure options, it'll take care of autoconf, autoheader and configure.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2318 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
+echo "Running autoconf..."
+autoconf || exit 1
+echo "Running autoheader..."
+autoheader || exit 1
+
+cd $ORIGDIR || exit $?
+
+$srcdir/configure "$@"
+
