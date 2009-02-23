f6d986abd65da01474f36dd77e8a3d676cce6fbf
nfurmento <nfurmento@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2009-02-23 13:17:26 +0000

    Optimize MTU usage for large messages
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2368 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- common/omx_wire.h
+++ common/omx_wire.h
@@ -67,19 +67,19 @@
 #  error OMX_MTU should be defined in non-wire-compatible mode
 # endif
 # if OMX_MTU < 2000 /* ugly since we can't compare with 1024+sizeof(header) */
-#  define OMX_PULL_REPLY_LENGTH_MAX		1024
+#  define OMX_PULL_REPLY_LENGTH_MAX		(OMX_MTU-sizeof(struct omx_pkt_head)-sizeof(struct omx_pkt_pull_reply))
 #  define OMX_MEDIUM_FRAG_LENGTH_MAX		(OMX_MTU-sizeof(struct omx_pkt_head)-sizeof(struct omx_pkt_medium_frag))
 #  define OMX_MEDIUM_FRAG_LENGTH_ROUNDUPSHIFT	11 /* the power-of-two above or equal to the max length */
 # elif OMX_MTU < 3000
-#  define OMX_PULL_REPLY_LENGTH_MAX		2048
+#  define OMX_PULL_REPLY_LENGTH_MAX		(OMX_MTU-sizeof(struct omx_pkt_head)-sizeof(struct omx_pkt_pull_reply))
 #  define OMX_MEDIUM_FRAG_LENGTH_MAX		(OMX_MTU-sizeof(struct omx_pkt_head)-sizeof(struct omx_pkt_medium_frag))
 #  define OMX_MEDIUM_FRAG_LENGTH_ROUNDUPSHIFT	12
 # elif OMX_MTU < 5000
-#  define OMX_PULL_REPLY_LENGTH_MAX		4096
+#  define OMX_PULL_REPLY_LENGTH_MAX		(OMX_MTU-sizeof(struct omx_pkt_head)-sizeof(struct omx_pkt_pull_reply))
 #  define OMX_MEDIUM_FRAG_LENGTH_MAX		(OMX_MTU-sizeof(struct omx_pkt_head)-sizeof(struct omx_pkt_medium_frag))
 #  define OMX_MEDIUM_FRAG_LENGTH_ROUNDUPSHIFT	13
 # else
-#  define OMX_PULL_REPLY_LENGTH_MAX		8192
+#  define OMX_PULL_REPLY_LENGTH_MAX		8192 /* FIXME: Should use OMX_MTU-sizeof(...) as above */
 #  define OMX_MEDIUM_FRAG_LENGTH_MAX		8192 /* FIXME: Should use OMX_MTU-sizeof(...) as above */
 #  define OMX_MEDIUM_FRAG_LENGTH_ROUNDUPSHIFT	13
 # endif
