a69b6f187f47980364ac2e05b1650e61bfeb002e
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2009-06-17 06:20:53 +0000

    omx_endpoint_info: Report information about all boards by default
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2615 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- tools/omx_endpoint_info.c
+++ tools/omx_endpoint_info.c
@@ -24,32 +24,86 @@
 
 #include "omx_lib.h"
 
-#define BID 0
-
 static void
 usage(int argc, char *argv[])
 {
   fprintf(stderr, "%s [options]\n", argv[0]);
-  fprintf(stderr, " -b <n>\tchange board id [%d]\n", BID);
+  fprintf(stderr, " -b <n>\tonly report board #<n>\n");
+  fprintf(stderr, " -a\treport all boards (default)\n");
   fprintf(stderr, " -v\tverbose messages\n");
 }
 
-int main(int argc, char *argv[])
+static void
+do_one_board(uint32_t board_index, uint32_t emax, int strict, int verbose)
 {
   struct omx_board_info board_info;
   struct omx_cmd_get_endpoint_info get_endpoint_info;
   char board_addr_str[OMX_BOARD_ADDR_STRLEN];
-  uint32_t board_index = BID;
+  omx_return_t ret;
+  int i, count, err;
+
+  /* get the board id */
+  ret = omx__get_board_info(NULL, board_index, &board_info);
+  if (ret != OMX_SUCCESS) {
+    if (strict)
+      fprintf(stderr, "Failed to read board #%d id, %s\n", board_index, omx_strerror(ret));
+    return;
+  }
+  omx__board_addr_sprintf(board_addr_str, board_info.addr);
+  printf("%s (board #%d name %s addr %s)\n",
+	 board_info.hostname, board_index, board_info.ifacename, board_addr_str);
+  printf("==============================================\n");
+
+  get_endpoint_info.board_index = board_index;
+  get_endpoint_info.endpoint_index = OMX_RAW_ENDPOINT_INDEX;
+
+  err = ioctl(omx__globals.control_fd, OMX_CMD_GET_ENDPOINT_INFO, &get_endpoint_info);
+  if (err < 0)
+    return;
+  OMX_VALGRIND_MEMORY_MAKE_READABLE(&get_endpoint_info, sizeof(get_endpoint_info));
+
+  if (!get_endpoint_info.info.closed)
+    printf("  raw\topen by pid %ld (%s)\n",
+	   (unsigned long) get_endpoint_info.info.pid, get_endpoint_info.info.command);
+  else if (verbose)
+    printf("  raw\tnot open\n");
+
+  count = 0;
+  for(i=0; i<emax; i++) {
+    get_endpoint_info.board_index = board_index;
+    get_endpoint_info.endpoint_index = i;
+
+    err = ioctl(omx__globals.control_fd, OMX_CMD_GET_ENDPOINT_INFO, &get_endpoint_info);
+    if (err < 0)
+      return;
+    OMX_VALGRIND_MEMORY_MAKE_READABLE(&get_endpoint_info, sizeof(get_endpoint_info));
+
+    if (!get_endpoint_info.info.closed) {
+      printf("  %d\topen by pid %ld (%s)\n", i,
+	     (unsigned long) get_endpoint_info.info.pid, get_endpoint_info.info.command);
+      count++;
+    } else if (verbose)
+      printf("  %d\tnot open\n", i);
+  }
+  printf("%d regular endpoints open (out of %d)\n", count, (unsigned) emax);
+  printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+  uint32_t board_index = OMX_ANY_NIC;
   omx_return_t ret;
   uint32_t emax;
   int verbose = 0;
-  int i, count, err;
   int c;
 
-  while ((c = getopt(argc, argv, "b:vh")) != -1)
+  while ((c = getopt(argc, argv, "b:avh")) != -1)
     switch (c) {
     case 'b':
       board_index = atoi(optarg);
+      break;
+    case 'a':
+      board_index = OMX_ANY_NIC;
       break;
     case 'v':
       verbose = 1;
@@ -72,49 +126,12 @@ int main(int argc, char *argv[])
   /* get endpoint max */
   emax = omx__driver_desc->endpoint_max;
 
-  /* get the board id */
-  ret = omx__get_board_info(NULL, board_index, &board_info);
-  if (ret != OMX_SUCCESS) {
-    fprintf(stderr, "Failed to read board #%d id, %s\n", board_index, omx_strerror(ret));
-    goto out;
+  if (board_index == OMX_ANY_NIC) {
+    for(board_index=0; board_index<omx__driver_desc->board_max; board_index++)
+      do_one_board(board_index, emax, 0, verbose);
+  } else {
+    do_one_board(board_index, emax, 1, verbose);
   }
-  omx__board_addr_sprintf(board_addr_str, board_info.addr);
-  printf("%s (board #%d name %s addr %s)\n",
-	 board_info.hostname, board_index, board_info.ifacename, board_addr_str);
-  printf("==============================================\n");
-
-  get_endpoint_info.board_index = board_index;
-  get_endpoint_info.endpoint_index = OMX_RAW_ENDPOINT_INDEX;
-
-  err = ioctl(omx__globals.control_fd, OMX_CMD_GET_ENDPOINT_INFO, &get_endpoint_info);
-  if (err < 0)
-    goto out;
-  OMX_VALGRIND_MEMORY_MAKE_READABLE(&get_endpoint_info, sizeof(get_endpoint_info));
-
-  if (!get_endpoint_info.info.closed)
-    printf("  raw\topen by pid %ld (%s)\n",
-	   (unsigned long) get_endpoint_info.info.pid, get_endpoint_info.info.command);
-  else if (verbose)
-    printf("  raw\tnot open\n");
-
-  count = 0;
-  for(i=0; i<emax; i++) {
-    get_endpoint_info.board_index = board_index;
-    get_endpoint_info.endpoint_index = i;
-
-    err = ioctl(omx__globals.control_fd, OMX_CMD_GET_ENDPOINT_INFO, &get_endpoint_info);
-    if (err < 0)
-      goto out;
-    OMX_VALGRIND_MEMORY_MAKE_READABLE(&get_endpoint_info, sizeof(get_endpoint_info));
-
-    if (!get_endpoint_info.info.closed) {
-      printf("  %d\topen by pid %ld (%s)\n", i,
-	     (unsigned long) get_endpoint_info.info.pid, get_endpoint_info.info.command);
-      count++;
-    } else if (verbose)
-      printf("  %d\tnot open\n", i);
-  }
-  printf("%d regular endpoints open (out of %d)\n", count, (unsigned) emax);
 
   return 0;
 
