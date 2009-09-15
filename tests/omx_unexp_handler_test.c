5b22b6eb8216963553200f7492488c80bfbe7a62
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2009-09-15 14:55:15 +0000

    tests: improve omx_unexp_handler_test to properly check data_if_available and the reported length in the handler, as well as properly checking the requests' completion status
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2742 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- tests/omx_unexp_handler_test.c
+++ tests/omx_unexp_handler_test.c
@@ -23,7 +23,9 @@
 
 #include "open-mx.h"
 
-static int discard = 0;
+static int discard;
+static int sync;
+static int length;
 
 static omx_unexp_handler_action_t
 unexp_handler(void *context, omx_endpoint_addr_t source,
@@ -32,6 +34,21 @@ unexp_handler(void *context, omx_endpoint_addr_t source,
 {
   omx_endpoint_t ep = context;
   omx_return_t ret;
+
+  /* check the message length */
+  assert(msg_length == length);
+
+  /* check that async message with a single frag have data available */
+  if (sync || length > 4096)
+    assert(!data_if_available);
+  else
+    assert(data_if_available);
+
+  if (data_if_available) {
+    int i;
+    for(i=0; i<msg_length; i++)
+      assert(((char*)data_if_available)[i] == 'a' + (i%26));
+  }
 
   if (discard) {
     printf("handler discarding directly\n");
@@ -43,18 +60,93 @@ unexp_handler(void *context, omx_endpoint_addr_t source,
   }
 }
 
+static void
+one_length(omx_endpoint_t ep, omx_endpoint_addr_t addr, char *buffer)
+{
+  omx_return_t ret;
+  omx_request_t req;
+  omx_status_t status;
+  uint32_t result;
+
+  printf("unexpected handler not discarding\n");
+  discard = 0;
+
+  sync = 0;
+  printf("posting isend %d\n", length);
+  ret = omx_isend(ep, buffer, length, addr, 0, NULL, &req);
+  assert(ret == OMX_SUCCESS);
+  printf("waiting for completion\n");
+  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
+  assert(ret == OMX_SUCCESS);
+  assert(result);
+  if (length > 32768) /* large messages are sync and thus truncated by discarding */
+    assert(status.code == OMX_MESSAGE_TRUNCATED);
+  else
+    assert(status.code == OMX_SUCCESS);
+  printf("isend completed\n");
+
+  sync = 1;
+  printf("posting issend %d\n", length);
+  ret = omx_issend(ep, buffer, length, addr, 0, NULL, &req);
+  assert(ret == OMX_SUCCESS);
+  printf("waiting for completion\n");
+  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
+  assert(ret == OMX_SUCCESS);
+  assert(result);
+  if (length) /* non-empty sync messages truncated by 0-byte recv */
+    assert(status.code == OMX_MESSAGE_TRUNCATED);
+  else
+    assert(status.code == OMX_SUCCESS);
+  printf("issend completed\n");
+
+  printf("unexpected handler discarding directly\n");
+  discard = 1;
+
+  sync = 0;
+  printf("posting isend %d\n", length);
+  ret = omx_isend(ep, buffer, length, addr, 0, NULL, &req);
+  assert(ret == OMX_SUCCESS);
+  printf("waiting for completion\n");
+  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
+  assert(ret == OMX_SUCCESS);
+  assert(result);
+  if (length > 32768) /* large messages are sync and thus truncated by discarding */
+    assert(status.code == OMX_MESSAGE_TRUNCATED);
+  else
+    assert(status.code == OMX_SUCCESS);
+  printf("isend completed\n");
+
+  sync = 1;
+  printf("posting issend %d\n", length);
+  ret = omx_issend(ep, buffer, length, addr, 0, NULL, &req);
+  assert(ret == OMX_SUCCESS);
+  printf("waiting for completion\n");
+  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
+  assert(ret == OMX_SUCCESS);
+  assert(result);
+  if (length) /* non-empty sync messages truncated by discarding */
+    assert(status.code == OMX_MESSAGE_TRUNCATED);
+  else
+    assert(status.code == OMX_SUCCESS);
+  printf("issend completed\n");
+}
+
 int
 main(int argc, char *argv[])
 {
   omx_return_t ret;
   omx_endpoint_t ep;
   omx_endpoint_addr_t addr;
-  omx_request_t req;
-  omx_status_t status;
-  uint32_t result;
+  char *buffer;
+  int i;
 
   putenv("OMX_DISABLE_SELF=1");
   putenv("OMX_DISABLE_SHARED=1");
+
+  buffer = malloc(1024*1024);
+  assert(buffer);
+  for(i=0; i<4096; i++)
+    buffer[i] = 'a' + (i%26);
 
   ret = omx_init();
   assert(ret == OMX_SUCCESS);
@@ -62,54 +154,26 @@ main(int argc, char *argv[])
   ret = omx_open_endpoint(OMX_ANY_NIC, OMX_ANY_ENDPOINT, 0x12345678, NULL, 0, &ep);
   assert(ret == OMX_SUCCESS);
 
+  (void) omx_set_error_handler(ep, OMX_ERRORS_RETURN);
+
   ret = omx_get_endpoint_addr(ep, &addr);
   assert(ret == OMX_SUCCESS);
 
   ret = omx_register_unexp_handler(ep, &unexp_handler, ep);
   assert(ret == OMX_SUCCESS);
 
-  printf("posting isend\n");
-  ret = omx_isend(ep, NULL, 0, addr, 0, NULL, &req);
-  assert(ret == OMX_SUCCESS);
-  printf("waiting for completion\n");
-  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
-  assert(ret == OMX_SUCCESS);
-  assert(result);
-  assert(status.code == OMX_SUCCESS);
-  printf("isend completed\n");
-
-  printf("posting issend\n");
-  ret = omx_issend(ep, NULL, 0, addr, 0, NULL, &req);
-  assert(ret == OMX_SUCCESS);
-  printf("waiting for completion\n");
-  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
-  assert(ret == OMX_SUCCESS);
-  assert(result);
-  assert(status.code == OMX_SUCCESS);
-  printf("issend completed\n");
-
-  printf("switching to unexpected handler discarding directly\n");
-  discard = 1;
-
-  printf("posting isend\n");
-  ret = omx_isend(ep, NULL, 0, addr, 0, NULL, &req);
-  assert(ret == OMX_SUCCESS);
-  printf("waiting for completion\n");
-  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
-  assert(ret == OMX_SUCCESS);
-  assert(result);
-  assert(status.code == OMX_SUCCESS);
-  printf("isend completed\n");
-
-  printf("posting issend\n");
-  ret = omx_issend(ep, NULL, 0, addr, 0, NULL, &req);
-  assert(ret == OMX_SUCCESS);
-  printf("waiting for completion\n");
-  ret = omx_wait(ep, &req, &status, &result, OMX_TIMEOUT_INFINITE);
-  assert(ret == OMX_SUCCESS);
-  assert(result);
-  assert(status.code == OMX_SUCCESS);
-  printf("issend completed\n");
+  length = 0; /* empty, data not available */
+  one_length(ep, addr, buffer);
+  length = 16; /* tiny */
+  one_length(ep, addr, buffer);
+  length = 100; /* small */
+  one_length(ep, addr, buffer);
+  length = 1024; /* medium one frag */
+  one_length(ep, addr, buffer);
+  length = 10000; /* medium two frag, data not available */
+  one_length(ep, addr, buffer);
+  length = 1024*1024; /* large, data not available */
+  one_length(ep, addr, buffer);
 
   omx_close_endpoint(ep);
   omx_finalize();
