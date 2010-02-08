381edb59d29b5be489ac27bf0fff0aecdbec95f4
bgoglin <bgoglin@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2010-02-08 14:41:11 +0000

    Update COPYING version from AC_INIT during autogen
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/trunk@2886 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- autogen.sh
+++ autogen.sh
@@ -10,8 +10,10 @@ echo "Running autoconf..."
 autoconf || exit 1
 echo "Running autoheader..."
 autoheader || exit 1
-echo "Updating open-mx.spec version..."
+echo "Updating COPYING version..."
 version=`sed -n /AC_INIT\(/,/\)/p configure.ac | tr -d '\n ' | cut -d, -f2`
+sed -e 's/^Open-MX: .*/Open-MX'${version}'/' -i COPYING
+echo "Updating open-mx.spec version..."
 sed -e 's/^Version: .*/Version: '${version}'/' -i open-mx.spec
 
 cd $ORIGDIR || exit $?
