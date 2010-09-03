5a612f4b1279da9616a85fb050e8aa5e0ccb5fe7
Brice Goglin <bgoglin@debian.org>
2010-09-03 11:47:13 +0200

    Do not mix expected and unexpected event queue full in the counters.

--- driver/linux/omx_event.c
+++ driver/linux/omx_event.c
@@ -239,7 +239,7 @@ omx_prepare_notify_unexp_event_with_recvq(struct omx_endpoint *endpoint,
 		dprintk(EVENT,
 			"Open-MX: Unexpected event queue full, no event slot available for endpoint %d\n",
 			endpoint->endpoint_index);
-		omx_counter_inc(endpoint->iface, EXP_EVENTQ_FULL);
+		omx_counter_inc(endpoint->iface, UNEXP_EVENTQ_FULL);
 		endpoint->userdesc->status |= OMX_ENDPOINT_DESC_STATUS_UNEXP_EVENTQ_FULL;
 		spin_unlock_bh(&endpoint->event_lock);
 		return -EBUSY;
@@ -281,7 +281,7 @@ omx_prepare_notify_unexp_events_with_recvq(struct omx_endpoint *endpoint,
 			dprintk(EVENT,
 				"Open-MX: Unexpected event queue full, no event slot available for endpoint %d\n",
 				endpoint->endpoint_index);
-			omx_counter_inc(endpoint->iface, EXP_EVENTQ_FULL);
+			omx_counter_inc(endpoint->iface, UNEXP_EVENTQ_FULL);
 			endpoint->userdesc->status |= OMX_ENDPOINT_DESC_STATUS_UNEXP_EVENTQ_FULL;
 			spin_unlock_bh(&endpoint->event_lock);
 			return -EBUSY;
