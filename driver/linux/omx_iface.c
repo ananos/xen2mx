dc855eb095e981b30e9c6a3301259ff16156f23b
Brice Goglin <bgoglin@inria.fr>
2012-01-29 23:12:54 +0100

    kfree() already checks whether the pointer is NULL (thanks to coccinelle)

--- driver/linux/omx_iface.c
+++ driver/linux/omx_iface.c
@@ -804,8 +804,7 @@ omx_ifnames_set_kp(const char *buf, struct kernel_param *kp)
 	} else {
 		/* If the 'ifnames' module parameter is given several times, we must first
 		 * free the allocated memory before reallocating it. */
-		if (omx_delayed_ifnames)
-			kfree (omx_delayed_ifnames);
+		kfree (omx_delayed_ifnames);
 
 		/* the module init isn't done yet, let the string be parsed later */
 		omx_delayed_ifnames = kstrdup(buf, GFP_KERNEL);
